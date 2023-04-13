5096fee5d3b898f1c5a5486ccc343d5e289a45d7
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-04-13 16:47:02 +0100

    Added emoji menu

--- frontend/src/components/feed/newPostForm/NewPostForm.js
+++ frontend/src/components/feed/newPostForm/NewPostForm.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewPostForm.css'
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
 
 const NewPostForm = ({newImg, newPost, handleNewPostChange, handleSubmit, handleImg}) => {
+  const [isPickerVisible, setPickerVisible] = useState(false);
+  const [currentEmoji, setCurrentEmoji] = useState(null);  
+
   
   return (
     <>
       <form className="new-post-form" onSubmit={handleSubmit} encType='multipart/form-data'>
+        <div>
+          <h1>{currentEmoji || 'select emoji'}</h1>
+          <button
+            onClick={() => setPickerVisible(!isPickerVisible)}
+          > 
+            open emoji picker
+          </button>
+
+          <div className={isPickerVisible ? 'd-block' : 'd-none'}>
+            <Picker
+              data={data}
+              previewPosition='none'
+              onEmojiSelect={(e) => { 
+                  setCurrentEmoji(e.native);
+                  setPickerVisible(!isPickerVisible);
+              }} 
+              />
+          </div>
+          
+
+        </div>
         <input type='text' id='post' className="text-field" placeholder="What do you have in mind?" value={newPost} onChange={handleNewPostChange} />
         <div id="upload-photo-btn-container">
           <input type='file' className="upload-photo-btn" accept=".png, .jpg, .jpeg" id='img' onChange={handleImg} />
